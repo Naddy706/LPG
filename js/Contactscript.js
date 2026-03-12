7ee273e43ea442cc26c16eaafe04d35f3243f668
Naddy706 <nadirkhosa08@gmail.com>
2026-03-11 21:11:02 -0700

    added  mailing function

--- js/Contactscript.js
+++ js/Contactscript.js
@@ -1,24 +1,49 @@
-document.getElementById("contact-form").addEventListener("submit", function(e){
-e.preventDefault();
+// document.getElementById("contact-form").addEventListener("submit", function(e){
+// e.preventDefault();
 
-emailjs.sendForm(
-'service_oamchfh',
-'template_9ihpk4i',
-this
-)
+// emailjs.sendForm(
+// 'service_oamchfh',
+// 'template_9ihpk4i',
+// this
+// )
 
-.then(()=>{
-alert("Message Sent!");
-this.reset();
-})
-.catch((err)=>{
-console.log(err);
-alert("Error sending message");
+// .then(()=>{
+// alert("Message Sent!");
+// this.reset();
+// })
+// .catch((err)=>{
+// console.log(err);
+// alert("Error sending message");
+// });
+
+// });
+
+
+
+
+
+emailjs.init("Qx-22jAlWmQPoSMGu");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_bc69219",
+    "template_klqbcpq",
+    this
+  ).then(function() {
+
+      document.getElementById("result").innerHTML =
+      "Message sent successfully!";
+
+  }, function(error) {
+
+      document.getElementById("result").innerHTML =
+      "Failed to send message.";
+
+  });
+
 });
-
-});
-
-
-
-
-
