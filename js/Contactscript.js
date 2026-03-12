af07da4f40b9355854d39b00e2c0201e9d45d2ce
Naddy706 <nadirkhosa08@gmail.com>
2026-03-11 21:15:35 -0700

    added  mailing function

--- js/Contactscript.js
+++ js/Contactscript.js
@@ -21,29 +21,3 @@
 
 
 
-
-emailjs.init("Qx-22jAlWmQPoSMGu");
-
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function(e) {
-
-  e.preventDefault();
-
-  emailjs.sendForm(
-    "service_bc69219",
-    "template_klqbcpq",
-    this
-  ).then(function() {
-
-      document.getElementById("result").innerHTML =
-      "Message sent successfully!";
-
-  }, function(error) {
-
-      document.getElementById("result").innerHTML =
-      "Failed to send message.";
-
-  });
-
-});
